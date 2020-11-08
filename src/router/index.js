90c72809a869897da256305d06841d2bad92c4d8
A2213 <2213070979@qq.com>
2020-11-08 13:30:46 +0800

    add createComponents

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import MovieRouter from './movie'
+import CinemaRouter from './cinema'
+import MineRouter from './mine'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
 
-const router = new VueRouter({
+
+export default new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes:[
+    //一级路由
+    MovieRouter,
+    CinemaRouter,
+    MineRouter,
+    //重定向
+    {
+      path:"/",
+      redirect:"/movie"
+    }
+  ]
 })
-
-export default router
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <Header title="我的喵喵"></Header>
+        <div id="content">
+            <Lonin></Lonin>
+        </div>
+        <TabBar></TabBar>
+    </div>
+</template>
+
+<script>
+import Header from "@/components/Header"
+import TabBar from "@/components/TabBar"
+import Lonin from "@/components/Login"
+export default {
+    name:"Mine",
+    components:{
+        Header,
+        TabBar,
+        Lonin
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
